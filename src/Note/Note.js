d5456d6ff59dbb50351c3ac138e04c92d26ec375
Olivia Davis <olivia.davis593@gmail.com>
2020-02-25 11:14:48 -0600

    implement design and routing

---
+++ src/Note/Note.js
@@ -0,0 +1,35 @@
+import React, { Component} from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Note.css';
+
+class Note extends Component {
+    render() {
+        const { name, id, modified } = this.props; 
+        return(
+            <div className='Note'>
+                <h2 className='Note__title'>
+                    <Link to={`/note/${id}`}>
+                        {name}
+                    </Link>
+                </h2>
+                <button className='Note__delete' type='button'>
+                    <FontAwesomeIcon icon='trash-alt' />
+                    {' '}
+                    remove
+                </button>
+                <div className='Note__dates'>
+                    <div className='Note__dates-modified'>
+                        Modified
+                        {' '}
+                        <span className='Date'>
+                            {modified}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Note; 
