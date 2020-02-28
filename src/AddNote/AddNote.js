1324889bfd638d9f28433dd7af8f580eec14ce8c
Olivia Davis <olivia.davis593@gmail.com>
2020-02-28 12:48:56 -0600

    implement addNote

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -22,7 +22,23 @@ class AddNote extends Component {
 
     static contextType = NoteContext; 
 
-    
+
+    componentWillMount() {
+        const currentFolder = this.context.folders.find(folder => 
+        `/addNote/folder/${folder.id}` === this.props.location)
+
+        if(currentFolder) {
+            const otherFolders = this.context.folders.filter(folder => folder.id !== currentFolder.id)
+            this.setState({
+                currentFolder,
+                folder_id: currentFolder.id,
+                folderOptions: otherFolders, 
+                selectedFolder: currentFolder,
+            });
+        } else {
+            this.setState({folderOptions: this.context.folders});
+        }
+    } 
 
     updateName(note_name) {
         this.setState({note_name})
@@ -120,22 +136,3 @@ class AddNote extends Component {
 
 export default AddNote; 
 
-
-/*
-componentWillMount() {
-        const currentFolder = this.context.folders.find(folder => 
-        `/addNote/folder/${folder.id}` === this.props.location)
-
-        if(currentFolder) {
-            const otherFolders = this.context.folders.filter(folder => folder.id !== currentFolder.id)
-            this.setState({
-                currentFolder,
-                folder_id: currentFolder.id,
-                folderOptions: otherFolders, 
-                selectedFolder: currentFolder,
-            });
-        } else {
-            this.setState({folderOptions: this.context.folders});
-        }
-    } 
-*/
