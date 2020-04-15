429a5e211b5953939d7efe67b1a6c34d76f9a8e7
Olivia Davis <olivia.davis593@gmail.com>
2020-04-15 12:53:27 -0500

    configure server to comply with client

---
+++ src/FolderList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import STORE from './store';
+import { Link } from 'react-router-dom';
+
+function FolderList() {
+    return(
+        <section>
+            <ul>
+                {STORE.folders.map(folder =>
+                <li key={folder.id}>
+                    <Link to={`/api/folders/${folder.id}`}>
+                        {folder.name}
+                    </Link>
+                </li>    
+                )}
+            </ul>
+        </section>
+    )
+}
+
+export default FolderList; 
